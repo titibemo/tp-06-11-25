ec7d4622dee1495528f4a0877a0c28fd3a7b373b
Thierry Wemeaux <levendeurlillois@hotmail.fr>
2025-11-06 10:05:19 +0100

    (feat - branch updatemovie): fonction updatemovie in write -> manip_data

--- write/manip_data.py
+++ write/manip_data.py
@@ -4,3 +4,10 @@ def addmovie():
     user_genre_movie = input("Choisissez le genre du film: ")
     user_age_limit = input("Choisissez l'age limit du film: ")
     return user_title_movie, user_annee_prod_movie, user_genre_movie, user_age_limit
+
+def updatemovie(idMovie):
+    user_title_movie = input("modification titre - Choisissez le nom de votre film ")
+    user_annee_prod_movie = input("modification année - Choisissez l'année de production: ")
+    user_genre_movie = input("modification genre - Choisissez le genre du film: ")
+    user_age_limit = input("modification age - Choisissez l'age limit du film: ")
+    return user_title_movie, user_annee_prod_movie, user_genre_movie, user_age_limit
