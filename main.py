ec3791ffa0b2246bfad85ac358fa421f26524bdc
Thierry Wemeaux <levendeurlillois@hotmail.fr>
2025-11-06 10:25:44 +0100

    (feat)add main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+from write.models.Movie import Movie
+
+def main():
+    pass
+    # all_movie = [
+    #     Movie("a", "b", "c", "d"),
+    # ]
+
+
+    
+
+    # while True:
+    #     user_input = input("Que voulez vous faire ? :")
+    #     match user_input:
+    #         case "1":
+    #             pass
+    #             # show_book(all_books)
+    #             # user_input = int(input("➡️  quel livre Voulez-vous CONSULTER ? "))
+    #             # all_books[user_input -1].consulter()             
+    #         case "2":
+    #             pass
+    #             # show_book(all_books)
+    #             # user_input = int(input("➡️  quel livre ou magazine voulez vous EMPRUNTER ? "))
+    #             # all_books[user_input -1].emprunter()
+    #         case "3":
+    #             pass
+    #             # show_book(all_books)
+    #             # user_input = int(input("➡️  quel livre ou magazine voulez vous RESTITUER ? "))
+    #             # all_books[user_input -1].rendre()
+    #         case "0":
+    #             pass
+    #             # print("🖐️  Merci et bonne journée")
+    #             # exit()
+
+if __name__ == '__main__':
+    main()
