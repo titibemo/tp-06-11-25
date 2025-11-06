89271fdb0ed2baa32699b157bc9cf1d0698a777d
Thierry Wemeaux <levendeurlillois@hotmail.fr>
2025-11-06 14:44:14 +0100

    (feat exceptions - branch exception): add exceptions

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from write.models.Movie import Movie
 from read.read_data import read_movie
 from write.manip_data import addmovie, updatemovie, deletemovie
-from write.exceptions import InvalidAgeLimitException, InvalidGenreException, InvalidTitleException, InvalidYearException
+from write.exceptions import InvalidNumberException, InvalidAgeLimitException, InvalidGenreException, InvalidTitleException, InvalidYearException
 
+##### nav
 def nav():
     print("====== GESTION BIBLIOTHEQUE ======")
     print("====== 1. Voir les films ======")
@@ -11,15 +12,54 @@ def nav():
     print("====== 4. supprimer un film ======")
     print("====== 0. Quitter ======")
 
-def main():
-    pass
-    # all_movie = [
-    #     Movie("a", "b", "c", "d"),
-    # ]1
+##### exception
+def is_title_valid():
+    user_title_movie = input("Choisissez le nom de votre film ")
 
-
+    if user_title_movie.isspace():
+        raise InvalidTitleException.InvalidTitleException("Le titre n'est un titre !") 
+    else:
+        return user_title_movie
     
+def is_annee_valid():
+    user_annee_prod_movie = input("Choisissez l'année de production: ")
 
+    if user_annee_prod_movie.isspace():
+        raise InvalidTitleException.InvalidTitleException("l'année n'est pas valide !") 
+    elif not user_annee_prod_movie.isdigit():
+        raise InvalidTitleException.InvalidTitleException("l'année n'est pas valide !") 
+    else:
+        return int(user_annee_prod_movie)
+    
+def is_genre_valid():
+    user_genre_movie = input("Choisissez le GENRE de votre film ")
+
+    if user_genre_movie.isspace():
+        raise InvalidTitleException.InvalidTitleException("Le genre n'ets pas bon !") 
+    elif not user_genre_movie.isalpha():
+        raise InvalidTitleException.InvalidTitleException("l'age n'est pas valide !") 
+    else:
+        return user_genre_movie
+    
+def is_age_valid():
+    user_age_limit = input("Choisissez le nom de votre film ")
+
+    if user_age_limit.isspace():
+        raise InvalidTitleException.InvalidTitleException("l'age n'est pas valide !") 
+    elif not user_age_limit.isdigit():
+        raise InvalidTitleException.InvalidTitleException("l'age n'est pas valide !") 
+    else:
+        return int(user_age_limit)
+
+def is_valid_number():
+    user_input = input("selectionner le numéro du film: ")
+
+    if user_input.isdigit():
+        return user_input
+    else:
+        raise InvalidNumberException.InvalidNumberException("La valeur inscrite n'est pas un numéro")
+
+def main():
     while True:
         nav()
         user_input = input("Que voulez vous faire ? :")
@@ -27,16 +67,40 @@ def main():
             case "1":
                 read_movie()        
             case "2":
-                addmovie()
+                try:
+                    user_title_movie = is_title_valid()
+                    user_annee_prod_movie = is_annee_valid()
+                    user_genre_movie = is_genre_valid()
+                    user_age_limit = is_age_valid()
+                except InvalidTitleException.InvalidTitleException as e:
+                    print(e)
+                except InvalidYearException.InvalidYearException as e:
+                    print(e)
+                except InvalidGenreException.InvalidGenreException as e:
+                    print(e)
+                except InvalidAgeLimitException.InvalidAgeLimitException as e:
+                    print(e)
+                else:
+                    addmovie(user_title_movie, user_annee_prod_movie, user_genre_movie, user_age_limit)
             case "3":
-                user_input = int(input("selectionner le numéro du film à modifier: "))
-                updatemovie(user_input)
+                try:
+                    user_input = is_valid_number()
+                except InvalidNumberException.InvalidNumberException as e:
+                    print(e)
+                else:
+                    updatemovie(user_input)
             case "4":
-                user_input = int(input("selectionner le numéro du film à effacer: "))
-                deletemovie(user_input)
+                try:
+                    user_input = is_valid_number()
+                except InvalidNumberException.InvalidNumberException as e:
+                    print(e)
+                else:
+                    deletemovie(user_input)
             case "0":
                 print("🖐️  Merci et bonne journée")
                 exit()
+            case _:
+                print("entrée inconnue...")
 
 if __name__ == '__main__':
     main()
---
+++ write/exceptions/InvalidNumberException.py
@@ -0,0 +1,7 @@
+class InvalidNumberException(Exception):
+    # def __init__(self, message):
+    #     self.message = message
+
+    # def display_exception(self):
+    #     print(f"{self.message}")
+    pass
